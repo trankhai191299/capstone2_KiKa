80a80799c9cd308b4c03d2ca4cb57d7aad72728d
khaitran <akashikazuo99@gmail.com>
2022-07-24 19:13:13 +0700

    Thêm Shoe.js trong js/model

---
+++ js/model/Shoe.js
@@ -0,0 +1,15 @@
+function Shoe() {
+  this.id = "";
+  this.name = "";
+  this.alias = "";
+  this.price = 0;
+  this.description = "";
+  this.size = [];
+  this.shortDescription = "";
+  this.quantity = 0;
+  this.deleted = true;
+  this.categories = [];
+  this.relatedProducts = [];
+  this.feature = true;
+  this.image = "";
+}

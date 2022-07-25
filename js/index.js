9c062744553b25ef8b15701c672985df46ad9c3d
khaitran <akashikazuo99@gmail.com>
2022-07-25 15:00:56 +0700

    change carousel to slick

--- js/index.js
+++ js/index.js
@@ -12,6 +12,20 @@ function getFeatureShoe() {
         console.log(e)
     })
 }
+//Get carousel shoe
+function getCarouselShoe(){
+  let promise = axios({
+    url:'https://shop.cyberlearn.vn/api/Product',
+        method: 'GET',
+  })
+  promise.then((result)=>{
+    // console.log(result.data)
+    renderCarouselShoe(result.data.content,"carousel-content");
+})
+promise.catch((e)=>{
+    console.log(e)
+})
+}
 //Show feature shoe data
 function renderFeatureShoe(arrSP,idBody){
     let html = ""
@@ -46,8 +60,37 @@ function renderFeatureShoe(arrSP,idBody){
     }
     document.getElementById(idBody).innerHTML = html
 }
-
+//Show carousel shoe data
+function renderCarouselShoe(arrSP,idBody){
+  let html = ""
+  for (let i in arrSP.slice(0,4)){
+    let shoe = arrSP[i];
+    let {id,image,name,shortDescription} = shoe;
+    html += `
+    <div class="slick-item">
+    <div class="row align-items-center">
+      <div class="col-8">
+        <div class="carousel-left">
+          <img src=${image} alt="..." height="auto" />
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="carousel-right">
+          <div class="shoe-info">
+            <h3 class="shoe-name">${name}</h3>
+            <p class="shoe-description">${shortDescription.length > 50?shortDescription.substring(0,50)+"...":shortDescription}</p>
+            <a class="btnBuy" href="./detail.html?getbyid=${id}">Buy Now</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    `
+  }
+  document.getElementById(idBody).innerHTML = html;
+}
 //------------------------------------------------
 window.onload = () => {
-  getFeatureShoe()
+  getFeatureShoe();
+  getCarouselShoe();
 } 
--- js/register.js
+++ js/register.js
@@ -3,10 +3,27 @@ import {User} from "./model/User.js"
 document.querySelector("#btnSubmit").onclick = function(e){
     e.preventDefault()
     let user = new User();
-    let arrInput = document.querySelectorAll("#registerFrm input")
+    let arrInput = document.querySelectorAll("#registerFrm .form-control");
+    let genderValue = document.querySelector("input[name='gender']:checked").value;
+    let mess = ""
     for (let input of arrInput){
         let {id,value} = input
-        user = {...user,[id]:value}
+        user[id] = value
+        user["gender"] = genderValue
     }
-    console.log(user)
+    if(user["password"]!==user["passwordConfirm"]){
+        return alert("Sai mật khẩu!")
+    }
+    let promise = axios({
+        url:"https://shop.cyberlearn.vn/api/Users/signup",
+        method:"POST",
+        data: user,
+    })
+    promise.then((result)=>{
+        mess = "Đăng ký thành công"
+        alert(mess);
+    })
+    promise.catch((e)=>{
+        console.log(e)
+    })
 }
